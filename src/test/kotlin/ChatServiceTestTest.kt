8ec3f8f3712e71e59604d6fd083193c63e97ad5f
Alexander <9922202@gmail.com>
2022-04-14 11:47:35 +0300

    Tests

---
+++ src/test/kotlin/ChatServiceTestTest.kt
@@ -0,0 +1,15 @@
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class ChatServiceTestTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
